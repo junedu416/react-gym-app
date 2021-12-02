7ff597018ef56568ec949552f4c140b709f9e593
Nam-fullstack <namnguyen13_@hotmail.com>
2021-12-02 22:19:39 +1100

    Creates css styled components for flexCol, flexRow, centered, middle.

---
+++ src/styled-components/index.js
@@ -0,0 +1,20 @@
+import { css } from 'styled-components';
+
+export const flexCol = css`
+  display: flex;
+  flex-direction: column;
+`
+export const centered = css`
+  justify-content: center;
+  align-items: center;
+`
+
+export const flexRow = css`
+  display: flex;
+  flex-direction: row;
+`
+
+export const middle = css`
+  position: absolute;
+  transform: translate(-50%, -50%);
+`
