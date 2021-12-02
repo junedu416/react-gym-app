666092e182db883bf0e8167434edafaac69d6368
Nam-fullstack <namnguyen13_@hotmail.com>
2021-12-02 22:07:58 +1100

    Creates form elements for login with state, will need to change logic for auth once backend in place.

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,9 +1,64 @@
 import React, { useState } from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Checkbox from "@material-ui/core/Checkbox";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 
 export const SignIn = (props) => {
+  const [state, setState] = React.useState({ checked: true });
+
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
+
+  const [membershipID, setMembershipID] = useState("");
+  const [login, setLogin] = useState(false);
+
+  // =======================================================
+  // Change out this logic for auth later
+  function handleSubmit(event) {
+    event.preventDefault();
+    setLogin(true);
+  }
+
   return (
     <>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          id="standard-basic"
+          label="Membership ID"
+          style={{ width: 300 }}
+        />
+        <TextField
+          id="standard-basic"
+          label="Password"
+          style={{ width: 300 }}
+        />
 
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={state.checked}
+              onChange={handleChange}
+              name="checked"
+              color="primary"
+            />
+          }
+          label="Remember Me"
+        />
+
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          size="large"
+          startIcon={<ExitToAppIcon />}
+          style={{ height: "55px" }}
+        >
+          Sign In
+        </Button>
+      </form>
     </>
   );
 };
--- src/pages/login/Register.js
+++ src/pages/login/Register.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import TextField from "@material-ui/core/TextField";
 
 export const Register = (props) => {
   return (
