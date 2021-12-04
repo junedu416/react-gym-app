4d9e16b1d6754a23c329cb835fa3100a3e7e2e7a
ahnemui <clh.oneil@gmail.com>
2021-12-04 14:59:52 +1100

    testing calendar onSelect

--- src/components/CalendarView.js
+++ src/components/CalendarView.js
@@ -1,28 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import "react-big-calendar/lib/css/react-big-calendar.css"
 import moment from 'moment';
+import { myEvents } from "../data/events-dummy";
 
 const CalendarView = () => {
 
+    const [message, setMessage] = useState("")
+
     const localizer = momentLocalizer(moment);
-    const myEvents = [
+    const events = [
         {
             title: "first event",
-            allDay: true,
-            start: moment("2021-12-05"),
-            end: moment("2021-12-06")
+            allDay: false,
+            start: new Date("2021-12-15"),
+            end: new Date("2021-12-17")
         }
     ]
+
+    const onClickEvent = () => {
+        if(message){
+            setMessage("")
+        } else {
+            setMessage("Clicked event")
+        }
+    }
 
     return(
         <div>
         <h1>Calendar</h1>
+        {message && <p>{message}</p>}
         <Calendar 
             localizer={localizer}
-            events={myEvents}
+            defaultView="week"
+            events={events}
             startAccessor="start"
             endAccessor="end"
+            onSelectEvent={onClickEvent}
             style={{height: "50vh"}}
         />
         </div>
