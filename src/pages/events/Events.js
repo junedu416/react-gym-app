8dca505cdccc32778dc61aba8dacefce9b3ad306
Nam-fullstack <namnguyen13_@hotmail.com>
2021-12-07 19:10:51 +1100

    Creates More Info and Book buttons for events.

--- src/pages/events/Events.js
+++ src/pages/events/Events.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
+import Info from "../../components/buttons/Info"
+import Book from "../../components/buttons/Book"
 
 export const Events = (props) => {
     
   return (
     <>
-
+      <Info />
+      <Book />
     </>
   );
 };
