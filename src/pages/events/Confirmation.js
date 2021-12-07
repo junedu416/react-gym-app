4ad52510fe843bcaa7384945a1c6d5205834eb7d
Nam-fullstack <namnguyen13_@hotmail.com>
2021-12-08 00:01:42 +1100

    Adds Cancel button.

--- src/pages/events/Confirmation.js
+++ src/pages/events/Confirmation.js
@@ -1,10 +1,16 @@
 import React from "react";
+import Cancel from "../../components/buttons/Cancel";
+import Confirm from "../../components/buttons/Confirm";
+import { Container } from "../../styled-components";
 
 export const BookingConfirmation = (props) => {
-    
   return (
-    <>
-
-    </>
+    <Container>
+      <p>Booking Confirmation Message</p>
+      <Container style={{ flexDirection: "row" }}>
+        <Confirm />
+        <Cancel />
+      </Container>
+    </Container>
   );
 };
--- src/pages/events/Events.js
+++ src/pages/events/Events.js
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
 import Info from "../../components/buttons/Info"
 import Book from "../../components/buttons/Book"
-import Confirm from "../../components/buttons/Confirm";
 import { MainWindow } from "../../styled-components";
+import { BookingConfirmation } from "./Confirmation";
 
 export const Events = (props) => {
     
   return (
+    <>
     <MainWindow>
       <Info />
       <Book />
-      <Confirm />
     </MainWindow>
+    <BookingConfirmation />
+    </>
   );
 };
