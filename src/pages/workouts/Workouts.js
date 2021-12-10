5360aaab1bb967d9bc7cc56333e2953ab3626f80
Nam <namnguyen13_@hotmail.com>
2021-12-11 03:27:42 +1100

    Adds create workout button.

--- src/pages/workouts/Workouts.js
+++ src/pages/workouts/Workouts.js
@@ -3,17 +3,27 @@ import EditButton from "../../components/buttons/Edit";
 import ViewExercises from "../../components/buttons/ViewExercises";
 import StartWorkout from "../../components/buttons/StartWorkout";
 import { Container, Heading, MainWindow } from "../../styled-components";
-import { Exercises } from "./Exercises";
+import { WorkoutCardStyling } from "../../styled-components/workouts";
+import CreateWorkout from "../../components/buttons/CreateWorkout";
 
 export const Workouts = (props) => {
+  const workoutList = ["Workout A", "Workout B", "Workout C"];
+
   return (
     <MainWindow>
       <Container>
         <Heading>Workouts</Heading>
         <EditButton />
+        <Container>
+          {workoutList.map((workout) => {
+            <WorkoutCardStyling>
+              {workout}
+            </WorkoutCardStyling>
+          })}
+        </Container>
         <StartWorkout />
         <p>Trainer Workouts</p>
-        <p>Create Workout</p>
+        <CreateWorkout />
         <ViewExercises />
       </Container>
     </MainWindow>
---
+++ src/styled-components/workouts.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import { flexbox, centered, mt, pt, shadow } from "./mixins";
+
+export const WorkoutCardStyling = styled.div`
+  width: 300px;
+  height: 250px;
+  border: 1px solid rgba(150, 150, 150, 0.25);
+  border-radius: 5px;
+  ${flexbox};
+  ${shadow};
+`
