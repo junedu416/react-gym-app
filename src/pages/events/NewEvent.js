2718edccf164c3fa893c44dfc6f78d0b01182366
ahnemui <clh.oneil@gmail.com>
2021-12-14 00:38:49 +1100

    reformats events form

--- src/pages/events/NewEvent.js
+++ src/pages/events/NewEvent.js
@@ -9,24 +9,21 @@ import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
+import { Input } from '@mui/material';
 
 // Date Selection
 import AdapterDateFns from "@mui/lab/AdapterDateFns";
 import LocalizationProvider from "@mui/lab/LocalizationProvider";
-import DatePicker from "@mui/lab/DatePicker";
 import MobileDatePicker from "@mui/lab/MobileDatePicker";
-import DesktopDatePicker from "@mui/lab/DesktopDatePicker";
 
 // Time Selection
-import TimePicker from "@mui/lab/TimePicker";
 import MobileTimePicker from "@mui/lab/MobileTimePicker";
-import DesktopTimePicker from "@mui/lab/DesktopTimePicker";
+
 
 export const NewEvent = (props) => {
-  const [eventType, setEventType] = useState('');
-  const [value, setValue] = useState(new Date());
-  const [time, setTime] = useState(new Date("2021-12-08T00:00:00.000Z"));
-  const [message, setMessage] = useState('');
+  const [start, setStart] = useState(new Date());
+  const [end, setEnd] = useState(new Date());
+  // const [time, setTime] = useState(new Date("2021-12-08T00:00:00.000Z"));
 
   const initialValues = {
     name: '',
@@ -44,96 +41,75 @@ export const NewEvent = (props) => {
     })
   };
 
-  const handleTyping = (event) => {
-    setMessage(event.target.value);
-  };
-
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log("submitted")
-    console.log(formValues, value)
+    const infoToSend = {
+      ...formValues,
+      start: start,
+      end: end
+    }
+    console.log(infoToSend);
   }
 
   return (
     <MainWindow>
       <Heading>Create Event</Heading>
       <form onSubmit={handleSubmit}>
-      <FormControl required sx={{ m: 1, minWidth: 480 }} >
-      <TextField
-        id="outlined-basic"
-        label="Event Name"
-        variant="outlined"
-        sx={{ minWidth: 480, mb: 3 }}
-        name="name"
-        onChange={handleChange}
-      />
-      {/* <FormControl required sx={{ m: 1, minWidth: 480 }}> */}
-        <InputLabel id="demo-simple-select-required-label">
-          Event Type
-        </InputLabel>
-        <Select
-          labelId="demo-simple-select-required-label"
-          id="demo-simple-select-required"
-          // value={eventType}
-          value={formValues.category}
+        <TextField
+          id="outlined-basic"
+          label="Event Name"
+          variant="outlined"
+          sx={{ minWidth: 480, mb: 3 }}
+          name="name"
+          onChange={handleChange}
+          required
+        />
+  
+        <TextField
+          id="outlined-select-event-type"
+          select
           label="Event Type"
+          value={formValues.category}
           onChange={handleChange}
           name="category"
-        >
-          <MenuItem value="class">Class</MenuItem>
-          <MenuItem value="competition">Competition</MenuItem>
-          {/* <MenuItem value="social">Social</MenuItem> */}
-        </Select>
-        <FormHelperText>Required *</FormHelperText>
-      </FormControl>
+          required
+          helperText="Please select the event type">
+            <MenuItem key="class" value="class">Class</MenuItem>
+            <MenuItem key="competition" value="competition">Competition</MenuItem>
+            <MenuItem key="personal training" value="personal training">Personal Training</MenuItem>
+        </TextField>
 
-      {/* Date Selection */}
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <Container style={{flexDirection: "row"}}>
 
-        <Stack spacing={2} mr={2} my={4}>
-          <MobileDatePicker
-            label="For mobile"
-            value={value}
-            onChange={(newValue) => {
-              setValue(newValue);
-            }}
-            renderInput={(params) => <TextField style={{marginBottom: "20px"}} {...params} />}
-          />
-          {/* <DatePicker
-            disableFuture
-            label="Responsive"
-            openTo="year"
-            views={["year", "month", "day"]}
-            value={value}
-            onChange={(newValue) => {
-              setValue(newValue);
-            }}
-            // value={formValues.startTime}
-            // name="startTime"
-            // onChange={handleChange}
-            renderInput={(params) => <TextField {...params} />}
-          /> */}
-        </Stack>
+        {/* Date Selection */}
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <Container style={{flexDirection: "row"}}>
 
-        <Stack spacing={2}>
-          <MobileTimePicker
-            label="For mobile"
-            value={value}
-            onChange={(newValue) => {
-              setValue(newValue);
-            }}
-            renderInput={(params) => <TextField style={{marginBottom: "20px"}} {...params} />}
-          />
-          {/* <DesktopTimePicker
-            label="For desktop"
-            value={value}
-            onChange={(newValue) => {
-              setValue(newValue);
-            }}
-            renderInput={(params) => <TextField {...params} />}
-          /> */}
-        </Stack>
+            <Stack spacing={2} mr={2} my={4}>
+              <MobileDatePicker
+                label="Start Date"
+                value={start}
+                onChange={(newValue) => {setStart(newValue);}}
+                renderInput={(params) => <TextField style={{marginBottom: "20px"}} {...params} />}/>
+              <MobileDatePicker
+                label="End Date"
+                value={end}
+                onChange={(newValue) => {setEnd(newValue);}}
+                renderInput={(params) => <TextField style={{marginBottom: "20px"}} {...params} />}/>
+            </Stack>
+
+            <Stack spacing={2}>
+              <MobileTimePicker
+                label="Start Time"
+                value={start}
+                onChange={(newValue) => {setStart(newValue);}}
+                renderInput={(params) => <TextField style={{marginBottom: "20px"}} {...params} />}/>
+              <MobileTimePicker
+                label="End Time"
+                value={end}
+                onChange={(newValue) => {setEnd(newValue);}}
+                renderInput={(params) => <TextField style={{marginBottom: "20px"}} {...params} />}/>
+            </Stack>
         </Container>
       </LocalizationProvider>
 
@@ -145,14 +121,27 @@ export const NewEvent = (props) => {
           maxRows={4}
           value={formValues.description}
           name="description"
-          // onChange={handleTyping}
           onChange={handleChange}
           sx={{ minWidth: 480 }}
+          required/>
+      
+      {/* <InputLabel for="spotsAvailable">Spots Available</InputLabel> */}
+      <TextField 
+        id="outlined-basic"
+        variant="outlined"
+        label="Spots Available"
+        type="number" 
+        name="spotsAvailable"
+        inputProps={{ min: "1", step: "1" }} 
+        required
+        onChange={handleChange}
+        value={formValues.spotsAvailable}
+        defaultValue="1"
         />
+
 
       <AttachmentIcon /> <span>Attach Photo</span>
         <CreateEvent />
-      {/* </FormControl> */}
       </form>
     </MainWindow>
   );
