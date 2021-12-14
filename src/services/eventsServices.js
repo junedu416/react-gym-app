2912a25f6a44fff2b353bcd9b29bc2a0f1a62eab
ahnemui <clh.oneil@gmail.com>
2021-12-14 11:17:47 +1100

    adds functions to get events from backend

---
+++ src/services/eventsServices.js
@@ -0,0 +1,64 @@
+import gymApi from '../config/api';
+
+// connecting to the server routes to push/retrieve info
+
+// GET  all existing events from backend
+export const getAllEvents = async () => {
+    try {
+        const response = await gymApi.get('/events')
+        console.log(response)
+        return response
+    } catch (e) {
+        console.log(e)
+        throw e
+    }
+}
+
+// GET a single exising event
+export const getEventById = async (id) => {
+    try {
+        const response = await gymApi.get(`/events/${id}`)
+        console.log(response)
+        return response
+    } catch (e) {
+        console.log(e)
+        throw e
+    }
+}
+
+// POST new event i.e PT, Class or Comp (staff only)
+export const createNewEvent = async (eventObj) => {
+    try {
+        const response = await gymApi.post('/events', eventObj)
+        console.log(response)
+        return response.data
+    } catch(e) {
+        console.log(e)
+        throw e
+    }
+}
+
+
+// PUTS (update) existing event (staff only (?))
+export const editEvent = async (id, eventObj) => {
+    try {
+        const response = await gymApi.puts(`/events/${id}`, eventObj)
+        console.log(response)
+        return response.data
+    } catch (e) {
+        console.log(e)
+        throw e
+    }
+}
+
+// DELETES existing event (CREATOR OF THE EVENT ONLY)
+export const deleteEvent = async (id) => {
+    try {
+        const response = await gymApi.delete(`/events/${id}`)
+        console.log(response)
+        return response.data
+    } catch (e) {
+        console.log (e)
+    }
+}
+
