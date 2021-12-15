a0f0861ed71472a279828a31ff95cf943e137959
Nam <namnguyen13_@hotmail.com>
2021-12-15 16:56:23 +1100

    Adds dropdown select and conditional rendering for contact inquiries.

--- src/styled-components/contact.js
+++ src/styled-components/contact.js
@@ -3,7 +3,7 @@ import { flexbox, centered, popupMiddle, mt, pt } from './mixins';
 
 export const ContactSubheadings = styled.h3`
   font-size: 1.3rem;
-  margin: 0 0 5px 0;
+  margin: 30px 0 15px 0;
   align-self: flex-start;
   color: red;  
   ${flexbox};
