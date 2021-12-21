724e182befd5babcc45eab725b8a23dc4a2c8f19
Nam <namnguyen13_@hotmail.com>
2021-12-22 02:42:07 +1100

    Adds calc to factor in sidebar and displays modal in middle.

--- src/pages/home/Overview.js
+++ src/pages/home/Overview.js
@@ -1,24 +1,33 @@
 import React from "react";
 import { CardStyle } from "../../styled-components/dashboard.js";
-import { Container, Grid, MainWindow, SmallHeading } from "../../styled-components";
+import {
+  Container,
+  Grid,
+  MainWindow,
+  SmallHeading,
+} from "../../styled-components";
 import { useGlobalState } from "../../config/globalStore.js";
 
 export const Overview = (props) => {
-
-  const {store} = useGlobalState();
-  const {profile} = store;
+  const { store } = useGlobalState();
+  const { profile } = store;
 
   return (
-    <MainWindow>
-      <SmallHeading>Welcome back { profile ? profile.firstName : "user" }, here's your overview</SmallHeading>
-      <Grid>
-        <CardStyle />
-        <CardStyle />
-        <CardStyle />
-        <CardStyle />
-        <CardStyle />
-        <CardStyle />
-      </Grid>
+    <MainWindow verticalMiddle>
+      <Container>
+        <SmallHeading>
+          Welcome back {profile ? profile.firstName : "user"}, here's your
+          overview
+        </SmallHeading>
+        <Grid>
+          <CardStyle />
+          <CardStyle />
+          <CardStyle />
+          <CardStyle />
+          <CardStyle />
+          <CardStyle />
+        </Grid>
+      </Container>
     </MainWindow>
   );
 };
