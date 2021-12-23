69897b1a63a6b9718fd01d0852cd168d70fa8964
Nam <namnguyen13_@hotmail.com>
2021-12-24 05:02:27 +1100

    Adds drag'n'drop component with file validation, file size, remove file. Just need to setup image upload with Firebase.

--- src/pages/home/MyProfile.js
+++ src/pages/home/MyProfile.js
@@ -2,17 +2,17 @@ import React from "react";
 import { UploadButton } from "../../components/buttons/Upload";
 import { Container, Heading, MainWindow } from "../../styled-components/";
 import DropZone from "../../components/DropZone";
+// import "../../App.css";
 
 export const MyProfile = (props) => {
   return (
     <MainWindow>
       <Heading>My Profile</Heading>
       <Container>
-        <UploadButton />
-        <p className="title">React Drag and Drop Image Upload</p>
         <div className="content">
           <DropZone />
         </div>
+        {/* <UploadButton /> */}
       </Container>
     </MainWindow>
   );
