e87b5b2ad634de4d2e0ca616d7bbef9ba69d9c0b
Nam <namnguyen13_@hotmail.com>
2021-12-24 03:49:46 +1100

    Defines methods for dragOVer, dragEnter, dragLeave and fileDrop.

--- src/components/DropZone.js
+++ src/components/DropZone.js
@@ -2,10 +2,39 @@ import React from "react";
 import "./DropZone.css";
 
 const DropZone = () => {
+  const dragOver = (event) => {
+    event.preventDefault();
+  };
+
+  const dragEnter = (event) => {
+    event.preventDefault();
+  };
+
+  const dragLeave = (event) => {
+    event.preventDefault();
+  };
+
+  const fileDrop = (event) => {
+    event.preventDefault();
+    const files = event.dataTransfer.files;
+    console.log(files);
+  };
+
   return (
     <>
       <div className="container">
-        <div className="drop-container"></div>
+        <div
+          className="drop-container"
+          onDragOver={dragOver}
+          onDragEnter={dragEnter}
+          onDragLeave={dragLeave}
+          onDrop={fileDrop}
+        >
+          <div className="drop-message">
+            <div className="upload-icon"></div>
+            Drag & Drop files here or click to upload
+          </div>
+        </div>
       </div>
     </>
   );
