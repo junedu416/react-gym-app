a6763b312d5d7e18bd7be4ee5a5b4b6d924e5f82
June Du <junedu416@gmail.com>
2022-01-01 20:15:52 +1100

    add resolve button functionality

--- src/pages/contact/ViewReports.js
+++ src/pages/contact/ViewReports.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Moment from 'react-moment';
+import Moment from "react-moment";
 import { Heading, MainWindow } from "../../styled-components";
 import { useGlobalState } from "../../config/globalStore.js";
 
@@ -11,86 +11,86 @@ export const ViewReports = () => {
   const { profile } = store;
   const [reportList, setReportList] = useState([]);
   const [open, setOpen] = useState([]);
-  const [resolved, setResolved] = useState(false)
-
-//   const initialValues = {
-//     type:"",
-//     userId:"",
-//     description:"",
-//     resolved: false,
-//     resolvedBy: "",
-//     reportDate: null, 
-//     reportImage: null
-// }
-// const [reportValues, setReportValues] = useState(initialValues)
+  const [resolved, setResolved] = useState([]);
 
   useEffect(() => {
     const fetchReportsInfo = async () => {
       const reports = await getAllReports();
-      console.log(reports)
       for (let report of reports) {
-        console.log(report)
-        let repoterProfile = await getUserProfile(report.userId);  
-        const reporterFullName =
-          repoterProfile.firstName + " " + repoterProfile.lastName;
+        let repoterProfile = await getUserProfile(report.userId);
+        const reporterFullName = repoterProfile ?
+          (repoterProfile.firstName + " " + repoterProfile.lastName):"Unknown User";
         report.reporterFullName = reporterFullName;
       }
-      
+
       setReportList(reports);
     };
 
     fetchReportsInfo().catch(console.error);
-  },[]);
+  }, []);
 
-  
+  const handleImageBtn = (index) => {
+    if (open.includes(index)) {
+      setOpen(open.filter((sindex) => sindex !== index));
+    } else {
+      let newOpen = [...open];
+      newOpen.push(index);
+      setOpen(newOpen);
+    }
+  };
 
-  const handleImageBtn = (id) =>{
-    if (open.includes(id)) {
-        setOpen(open.filter(sid => sid !== id))
-       } else {
-        let newOpen = [...open]
-        newOpen.push(id)
-        setOpen(newOpen)
-       }
-  }
+  const [reportValues, setReportValues] = useState({});
+  const handleResolveBtn = async (index) => {
+    if (resolved.includes(index)) {
+      setResolved(resolved.filter((sindex) => sindex !== index));
+    } else {
+      let newResolved = [...open];
+      newResolved.push(index);
+      setResolved(newResolved);
+    }
 
-  const handleResolveBtn = async (id, report) => {
-    setResolved(!resolved)
-    console.log("resolve:", resolved)
     setReportValues({
-        ...reportValues,
-        type: report.type,
-        userId: report.userId,
-        description: report.description,
-        resolved: resolved,
-        resolvedBy: profile._Id,
-        reportDate: report.reportDate, 
-        reportImage: report.reportImage
-    })
+      ...reportList[index],
+      resolved: !!resolved.includes(index),
+      resolvedBy: resolved.includes(index)
+        ? `${profile.firstName} ${profile.lastName}`
+        : null,
+    });
+    console.log("reportValueToSend:", reportValues);
 
-    const request = await editReport(id, reportValues)
-    console.log(request)
-    
-  }
+    const request = await editReport(reportValues._id, reportValues);
+    return request;
+  };
 
   return (
     <MainWindow>
-      {console.log(reportList)}
       <Heading>View Reports</Heading>
       <ul>
-      {reportList.map((report, index) => {
-        return (
-          <li key={index}>
-            Type: {report.type}<br/>
-            Reporter Name: {report.reporterFullName}<br/>
-            Report Time: <Moment fromNow>{report.reportDate}</Moment><br/>
-            Description: {report.description}<br/>
-            <button onClick={()=>handleResolveBtn(report._id, report)}>{report.resolved? "Unresolved":"Issue Resolved"}</button>
-            <button onClick={()=> handleImageBtn(index)}>{open.includes(index) ? "Hide Photo":"Show Photo"}</button>
-            {open.includes(index) && <img src= {report.reportImage}/>}
-          </li>
-        );
-      })}
+        {reportList.map((report, index) => {
+          return (
+            <li key={index}>
+              Type: {report.type}
+              <br />
+              Reporter Name: {report.reporterFullName}
+              <br />
+              Report Time: <Moment fromNow>{report.reportDate}</Moment>
+              <br />
+              Description: {report.description}
+              <br />
+              <button onClick={() => handleResolveBtn(index)}>
+                {resolved.includes(index) ? "Unresolved" : "Issue Resolved"}
+              </button>
+              {resolved.includes(index) && (
+                <p> {report.resolvedBy} </p>
+              )}
+              <button onClick={() => handleImageBtn(index)}>
+                {open.includes(index) ? "Hide Photo" : "Show Photo"}
+              </button>
+              {open.includes(index) && <img src={report.reportImage} />}
+            </li>
+          );
+        })}
+        <br />
       </ul>
     </MainWindow>
   );
