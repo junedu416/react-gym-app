058b84ac74ec80d8cedc3ab3e49e80dc210d9a27
ahnemui <clh.oneil@gmail.com>
2022-01-04 01:38:06 +1100

    adds details to event show page including name of event creator

--- src/pages/events/ShowEvent.js
+++ src/pages/events/ShowEvent.js
@@ -1,19 +1,26 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useReducer} from 'react';
 import { useParams } from 'react-router-dom';
-import { MainWindow } from '../../styled-components'
-import { getEventById } from '../../services/eventsServices'
+import { MainWindow } from '../../styled-components';
+import { getEventById } from '../../services/eventsServices';
+import { showEventReducer } from '../../utils/showEvent-reducer';
+import Book from '../../components/buttons/Book';
 
 export const ShowEvent = () => {
     const {id} = useParams();
-    const [event, setEvent] = useState(null)
     const [loading, setLoading] = useState(true)
     const [errorMsg, setErrorMsg] = useState("");
+    const [event, dispatchEvent] = useReducer(showEventReducer, {})
 
     useEffect(() => {
         getEventById(id)
         .then((response) => {
             console.log("loaded response is: ", response)
-            setEvent(response)
+            dispatchEvent({type: 'setEvent', data: response})
+            dispatchEvent({
+                type: "setEventTimes", data: {
+                    startTime: response.startTime,
+                    endTime: response.endTime
+                }})
         }).then(() => setLoading(false))
         .catch((error) => {
             console.log(error)
@@ -22,6 +29,7 @@ export const ShowEvent = () => {
         })
     }, [id])
 
+
     return(
         <MainWindow>
             <h1>Show Event Page</h1>
@@ -29,7 +37,20 @@ export const ShowEvent = () => {
             {errorMsg && <p>{errorMsg}</p>}
             {event && 
                 <div>
-                    <h2>{event.name}</h2>
+                    <div>
+                        <h2>{event.name}</h2>
+                        <h3>{event.category}</h3>
+                        <h4>Event Listed by {event.creatorName}</h4>
+                        {event.eventImage ?  <img href={event.eventImage} alt={event.name}/> : <p>no image available</p>}
+                        <p>{event.description}</p>
+                        {(event.startDate !== event.endDate) && <p>{event.startDate} at {event.startTime} ~ {event.endDate} at {event.endTime}</p>}
+                        {(event.startDate === event.endDate) && <p>{event.startDate} from {event.startTime} ~ {event.endTime}</p>}
+                        {event.spotsAvailable !== 0 && <>
+                            <p>{event.spotsAvailable} {event.spotsAvailable === 1 ? "spot" : "spots"} left!</p>
+                            <Book></Book>
+                            </>}
+
+                    </div>
                 </div>
             }
         </MainWindow>
---
+++ src/utils/showEvent-reducer.js
@@ -0,0 +1,37 @@
+import moment from "moment";
+
+export const showEventReducer = (state, action) => {
+    switch(action.type) {
+        case "setEvent": {
+            return {
+                ...state,
+                name: action.data.name,
+                category: action.data.category,
+                creatorName: `${action.data.createdBy.firstName} ${action.data.createdBy.lastName}`,
+                description: action.data.description,
+                startTime: action.data.startTime,
+                endTime: action.data.endTime,
+                spotsAvailable: action.data.spotsAvailable,
+                registeredUsers: action.data.registeredUsers,
+                _id: action.data._id,
+                eventImage: action.data.eventImage
+            }
+        }
+        case "setEventTimes": {
+            const startDate = moment(action.data.startTime).format('Do [of] MMM')
+            const startTime = moment(action.data.startTime).format('h:mm A')
+            const endDate = moment(action.data.endTime).format('Do [of] MMM')
+            const endTime = moment(action.data.endTime).format('h:mm A')
+            return {
+                ...state,
+                startDate: startDate,
+                startTime: startTime,
+                endDate: endDate,
+                endTime: endTime
+            }
+        }
+        default: {
+            return state
+        }
+    }
+}
