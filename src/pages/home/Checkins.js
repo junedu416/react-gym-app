ceae3f13e838c50ef25e4d52d2016d8742d213cd
daniel-walters <waltersdaniel96@gmail.com>
2022-01-04 11:49:25 +1100

    checks if profile already checked in / out

--- src/pages/home/Checkins.js
+++ src/pages/home/Checkins.js
@@ -1,15 +1,35 @@
 import React from "react";
 import { Container, Heading, MainWindow } from "../../styled-components/";
 import { checkIn, checkOut } from "../../services/checkinServices";
+import { useGlobalState } from "../../config/globalStore";
 
 export const Checkins = () => {
 
+  const {store} = useGlobalState();
+  const {profile} = store;
+
   function handleCheckIn() {
-    checkIn().then(() => console.log("checked in"));
+    if (profile) {
+      if (!profile.checkedIn) {
+        checkIn({userId: profile.userId}).then(() => console.log("checked in"));
+      } else {
+        console.log("Already checked In");
+      }
+    } else {
+      console.log("not logged in");
+    }
   }
 
   function handleCheckOut() {
-    checkOut().then(() => console.log("checked out"));
+    if (profile) {
+      if (profile.checkedIn) {
+        checkOut({userId: profile.userId}).then(() => console.log("checked out"));
+      } else {
+        console.log("Already checked out")
+      }
+    } else {
+      console.log("not logged in");
+    }
   }
     
   return (
--- src/services/checkinServices.js
+++ src/services/checkinServices.js
@@ -1,17 +1,25 @@
 import gymApi from "../config/api";
 
-export async function checkIn() {
+export async function checkIn(user) {
     try {
-        const response = await gymApi.post("/checkin/check-in");
+        const response = await gymApi.post("/checkin/check-in", user, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
         return response.data;
     } catch(e) {
         console.log(e);
     }
 }
 
-export async function checkOut() {
+export async function checkOut(user) {
     try {
-        const response = await gymApi.post("/checkin/check-out");
+        const response = await gymApi.post("/checkin/check-out", user, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
         return response.data;
     } catch(e) {
         console.log(e);
