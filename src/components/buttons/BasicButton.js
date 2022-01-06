bee8e379830e69dc68a14450524b9f97dddf8a54
Nam <namnguyen13_@hotmail.com>
2022-01-07 01:10:15 +1100

    Adds default prop settings for Basic Button component, and also includes icons to be passed in props.

--- src/components/buttons/BasicButton.js
+++ src/components/buttons/BasicButton.js
@@ -1,19 +1,22 @@
 import Button from "@mui/material/Button";
 
 const BasicButton = (props) => {
+    // Variant types: contained, outlined, text (default)
     // colour options for buttons: primary, secondary, warning, error, success
     // size options: small, medium, large
-    const { btnFunction, text, color, size, style, disabled } = props;
+    const { btnFunction, text, color, size, style, disabled, variant, startIcon, endIcon } = props;
     const buttonStyle = {height: "55px", width: "116px"}
 
   return (
     <Button
-      variant="contained"
-      color={color}
-      size={size}
+      variant={ variant ? variant : "contained" }
+      color={ color ? color : "primary" }
+      size={ size ? size : "large" }
       style={{...buttonStyle, ...style}}
       onClick={btnFunction}
       disabled={disabled}
+      startIcon={startIcon}
+      endIcon={endIcon}
     >
       {text}
     </Button>
