aa2534f78ff7e78d3e251805d8f447d3bb927e58
Nam <namnguyen13_@hotmail.com>
2022-01-10 03:56:32 +1100

    Attempting to useReducer for filtering functionality, not working, will come back to this later.

--- src/pages/events/Calendar.js
+++ src/pages/events/Calendar.js
@@ -1,30 +1,69 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import ToggleButton from "@mui/material/ToggleButton";
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 import CalendarView from "../../components/CalendarView";
 import { Container } from "../../styled-components";
-import FilterAltIcon from "@mui/icons-material/FilterAlt";
 import ClickAwayListener from "@mui/material/ClickAwayListener";
-import Chip from "@mui/material/Chip";
+// import Chip from "@mui/material/Chip";
 import { FilterBox, FilterItem } from "../../styled-components/events";
-import { gymClasses, trainers, weekdays } from "../../data/events";
-import DoneIcon from "@mui/icons-material/Done";
+import { gymClasses, trainers, weekdays, competitionFilters, allFilters } from "../../data/events";
 import Button from "@mui/material/Button";
 import { styled } from "@mui/material/styles";
 import ArrowForwardIosSharpIcon from "@mui/icons-material/ArrowForwardIosSharp";
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import MuiAccordion from "@mui/material/Accordion";
 import MuiAccordionSummary from "@mui/material/AccordionSummary";
 import MuiAccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
-import { FilterHeading } from "../../components/FilterHeading";
 import BasicButton from "../../components/buttons/BasicButton";
-// import CloseIcon from '@mui/icons-material/Close';
+import FilterAltIcon from "@mui/icons-material/FilterAlt";
+import DoneIcon from "@mui/icons-material/Done";
 import CancelIcon from "@mui/icons-material/Cancel";
+import { id } from "date-fns/locale";
+// import CloseIcon from '@mui/icons-material/Close';
+// import { FilterHeading } from "../../components/FilterHeading";
 
 // const ListItem = styled('li')(({ theme }) => ({
 //   margin: theme.spacing(2),
 // }));
+
+const ACTIONS = {
+  ADD_FILTER: 'add-filter',
+  REMOVE_FILTER: 'remove-filter',
+  TOGGLE_FILTER: 'toggle-filter',
+}
+
+// function reducer(state, action) {
+//   switch (action.type) {
+//     case 'addFilter':
+//       return [...filterList, state.payload]
+//     case 'removeFilter':
+//       return filterList.filter(filter => filter.id !== state.payload.id)
+//     case 'toggle':
+//       return allFilters.map(filter => {
+//         if (filter.id === action.payload.id) {
+//           return { ...allFilters, filter: !filter.selected }
+//         }
+//         return filter
+//       })
+//     case 'reset':
+//       return []
+//     default:
+//       return state
+//   }
+// }
+
+function reducer(filterList, action) {
+  switch (action.type) {
+    case ACTIONS.ADD_FILTER: 
+      // return [...filterList, newFilter(action.payload.label)]
+    case ACTIONS.TOGGLE_FILTER:
+      return [];
+    case ACTIONS.REMOVE_FILTER:
+      return allFilters.filter(item => item.id !== action.payload.id)
+    default: 
+      return filterList
+  }
+}
 
 const Accordion = styled((props) => (
   <MuiAccordion disableGutters elevation={0} square {...props} />
@@ -70,39 +109,21 @@ const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
 export const Calendar = () => {
   const [eventSelect, setEventSelect] = useState("class");
   const [open, setOpen] = useState(false);
-  const [filters, setFilters] = useState([]);
-
   const [expanded, setExpanded] = useState("panel1");
+
+  
+  const [filter, setFilter] = useState("");
+
+  
+
+  const [filterList, dispatch] = useReducer(reducer, []);
+
+  function newFilter(name) {
+    return allFilters.filter(item => item.label === name) 
+  }
 
   const handleChange = (panel) => (event, newExpanded) => {
     setExpanded(newExpanded ? panel : false);
-  };
-
-  const classFilters = [
-    { key: 0, label: "Class" },
-    { key: 1, label: "Day" },
-    { key: 2, label: "Trainer" },
-    { key: 3, label: "Full Capacity" },
-  ];
-
-  const trainerFilters = [
-    { key: 0, label: "Trainer" },
-    { key: 1, label: "Day" },
-  ];
-
-  const competitionFilters = [
-    { key: 0, label: "Cardio" },
-    { key: 1, label: "Cycling" },
-    { key: 2, label: "Lifting" },
-    { key: 3, label: "Rowing" },
-    { key: 4, label: "Running" },
-    { key: 5, label: "Weight Loss" },
-  ];
-
-  const eventSelection = () => {
-    if (eventSelect === "class") return classFilters;
-    if (eventSelect === "competition") return competitionFilters;
-    if (eventSelect === "personal training") return trainerFilters;
   };
 
   const handleClick = () => {
@@ -118,7 +139,20 @@ export const Calendar = () => {
     else {
       setEventSelect(newEventSelect);
     }
-  };
+  }
+
+  function handleFilterSelect(e) {
+    dispatch({ type: ACTIONS.ADD_FILTER, payload: { name: filter } })
+    
+  }
+
+  // function toggleFilter(id) {
+  //   return allFilters.filter(item => {
+  //     if(item.id === action.payload.id) {
+  //       item.filter = !item.filter
+  //     }
+  //   }
+  // }
 
   // useEffect(() => {
   //   for (let i = 0; i < filters.length; i++) {
@@ -126,6 +160,13 @@ export const Calendar = () => {
   //   }
   //   setFilters(filters);
   // }, []);
+
+
+  // function selectFilter(key) {
+  //   const tempFilterList = [...filterList];
+  //   tempFilterList[key] = !tempFilterList[key];
+  //   setFilterList(tempFilterList);
+  // }
 
   return (
     <>
@@ -141,92 +182,101 @@ export const Calendar = () => {
               </Container>
               {open ? (
                 <FilterBox>
-                { eventSelect === "competition" ? (
-                  <Accordion
-                    expanded
-                    onChange={handleChange("panel3")}
-                    sx={{ borderBottomLeftRadius: "8px", borderBottomRightRadius: "8px"}}
-                  >
-                    <AccordionSummary>
-                      <Typography fontWeight="bold">Comp. Category</Typography>
-                    </AccordionSummary>
-                    <AccordionDetails sx={{ p: 1 }}>
-                      {competitionFilters.map((category, index) => {
-                        return (
-                          <FilterItem key={category.label}>
-                            <Typography>{category.label}</Typography>
-                            <DoneIcon color="success" />
-                          </FilterItem>
-                        );
-                      })}
-                    </AccordionDetails>
-                  </Accordion>) :
- (              <>
-                  <Accordion
-                    expanded={expanded === "panel1"}
-                    onChange={handleChange("panel1")}
-                    sx={{ borderTopLeftRadius: "8px", borderTopRightRadius: "8px"}}
-                  >
-                    <AccordionSummary>
-                      <Typography fontWeight="bold">Day</Typography>
-                    </AccordionSummary>
-                    <AccordionDetails sx={{ p: 1 }}>
-                      {weekdays.map((day) => {
-                        return (
-                          <FilterItem key={day.label}>
-                            <Typography>{day.label}</Typography>
-                            <DoneIcon color="success" />
-                          </FilterItem>
-                        );
-                      })}
-                    </AccordionDetails>
-                  </Accordion>
+                  {eventSelect === "competition" ? (
+                    <Accordion
+                      expanded
+                      onChange={handleChange("panel3")}
+                      sx={{
+                        borderBottomLeftRadius: "8px",
+                        borderBottomRightRadius: "8px",
+                      }}
+                    >
+                      <AccordionSummary>
+                        <Typography fontWeight="bold">
+                          Comp. Category
+                        </Typography>
+                      </AccordionSummary>
+                      <AccordionDetails sx={{ p: 1 }}>
+                        {competitionFilters.map((category, index) => {
+                          return (
+                            <FilterItem key={category.label}>
+                              <Typography>{category.label}</Typography>
+                              <DoneIcon color="success" />
+                            </FilterItem>
+                          );
+                        })}
+                      </AccordionDetails>
+                    </Accordion>
+                  ) : (
+                    <>
+                      <Accordion
+                        expanded={expanded === "panel1"}
+                        onChange={handleChange("panel1")}
+                        sx={{
+                          borderTopLeftRadius: "8px",
+                          borderTopRightRadius: "8px",
+                        }}
+                      >
+                        <AccordionSummary>
+                          <Typography fontWeight="bold">Day</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails sx={{ p: 1 }}>
+                          {weekdays.map((day) => {
+                            return (
+                              <FilterItem key={day.label} onClick={ () => {} }>
+                                <Typography>{day.label}</Typography>
+                                <DoneIcon color="success" />
+                              </FilterItem>
+                            );
+                          })}
+                        </AccordionDetails>
+                      </Accordion>
 
-                  { eventSelect === "class" && (
-                  <Accordion
-                    expanded={expanded === "panel2"}
-                    onChange={handleChange("panel2")}
-                  >
-                    <AccordionSummary>
-                      <Typography fontWeight="bold">Class</Typography>
-                    </AccordionSummary>
-                    <AccordionDetails sx={{ p: 1 }}>
-                      {gymClasses.map((groupClass, index) => {
-                        return (
-                          <FilterItem key={index}>
-                            <Typography>{groupClass.name}</Typography>
-                            <DoneIcon color="success" />
-                          </FilterItem>
-                        );
-                      })}
-                    </AccordionDetails>
-                  </Accordion>
-                  
+                      {eventSelect === "class" && (
+                        <Accordion
+                          expanded={expanded === "panel2"}
+                          onChange={handleChange("panel2")}
+                        >
+                          <AccordionSummary>
+                            <Typography fontWeight="bold">Class</Typography>
+                          </AccordionSummary>
+                          <AccordionDetails sx={{ p: 1 }}>
+                            {gymClasses.map((groupClass, index) => {
+                              return (
+                                <FilterItem key={groupClass.name} onClick={ {}}>
+                                  <Typography>{groupClass.name}</Typography>
+                                  <DoneIcon color="success" />
+                                </FilterItem>
+                              );
+                            })}
+                          </AccordionDetails>
+                        </Accordion>
+                      )}
+
+                      <Accordion
+                        expanded={expanded === "panel4"}
+                        onChange={handleChange("panel4")}
+                        sx={{
+                          borderBottomLeftRadius: "8px",
+                          borderBottomRightRadius: "8px",
+                        }}
+                      >
+                        <AccordionSummary>
+                          <Typography fontWeight="bold">Trainer</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails sx={{ p: 1 }}>
+                          {trainers.map((trainer) => {
+                            return (
+                              <FilterItem key={trainer.name}>
+                                <Typography>{trainer.name}</Typography>
+                                <DoneIcon color="success" />
+                              </FilterItem>
+                            );
+                          })}
+                        </AccordionDetails>
+                      </Accordion>
+                    </>
                   )}
-
-                  <Accordion
-                    expanded={expanded === "panel4"}
-                    onChange={handleChange("panel4")}
-                    sx={{ borderBottomLeftRadius: "8px", borderBottomRightRadius: "8px"}}
-                  >
-                    <AccordionSummary>
-                      <Typography fontWeight="bold">Trainer</Typography>
-                    </AccordionSummary>
-                    <AccordionDetails sx={{ p: 1 }}>
-                      {trainers.map((trainer) => {
-                        return (
-                          <FilterItem key={trainer.name}>
-                            <Typography>{trainer.name}</Typography>
-                            <DoneIcon color="success" />
-                          </FilterItem>
-                        );
-                      })}
-                    </AccordionDetails>
-                  </Accordion>
-
-  
-                  </>
-                )}
                 </FilterBox>
               ) : null}
             </Container>
@@ -250,9 +300,8 @@ export const Calendar = () => {
                 },
               }}
               style={{ height: "37px" }}
-
               // ADD FUNCTION HERE!!!!!!!!!
-              btnFunction={{   }}
+              btnFunction={{}}
             />
           }
         </Container>
@@ -296,5 +345,5 @@ export const Calendar = () => {
       </Container>
       <CalendarView eventCategory={eventSelect} />
     </>
-  );
-};
+  )
+}
