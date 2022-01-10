099d9aa2e9677e585aef0d077e914b7e923e6674
Nam <namnguyen13_@hotmail.com>
2022-01-10 17:16:02 +1100

    Adds props to customise Reusable alert variant, otherwise defaults to filled.

--- src/components/ReusableAlert.js
+++ src/components/ReusableAlert.js
@@ -5,12 +5,14 @@ import { useState } from "react";
 import { StyledAlert } from "../styled-components";
 
 export const ReusableAlert = (props) => {
-  const { severity, btnFunction, text, open } = props;
+  const { severity, btnFunction, text, open, variant } = props;
 
   return (
     <Collapse in={open}>
       <StyledAlert
         severity={severity}
+        variant={variant ? variant : "filled"}
+        sx={{ minWidth: "200px" }}
         action={
           <IconButton
             aria-label="close"
