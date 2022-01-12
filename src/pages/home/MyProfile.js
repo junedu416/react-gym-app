ed7217fe37ed9ded69e4294d8fa8e1d9fbcf9812
Nam <namnguyen13_@hotmail.com>
2022-01-12 16:06:41 +1100

    Aligns text to be as per wireframe.

--- src/pages/home/MyProfile.js
+++ src/pages/home/MyProfile.js
@@ -10,14 +10,12 @@ import {
   Text,
   TextBold,
 } from "../../styled-components/";
+import { ProfilePicture } from "../../components/ProfilePicture";
 import BasicButton from "../../components/buttons/BasicButton";
 import PhotoCameraIcon from "@mui/icons-material/PhotoCamera";
-import { ProfileImage } from "../../styled-components/profile";
-// import DropZone from "../../components/DropZone";
 
-import { useTheme } from '@mui/material/styles';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import { ProfilePicture } from "../../components/ProfilePicture";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 export const MyProfile = (props) => {
   useRedirectUnauthorisedUser();
@@ -31,39 +29,48 @@ export const MyProfile = (props) => {
   const [isFilePicked, setIsFilePicked] = useState(false);
 
   const theme = useTheme();
-  const matches = useMediaQuery(theme.breakpoints.down('sm'));
+  const matches = useMediaQuery(theme.breakpoints.down("sm"));
 
   const selectImage = (event) => {
     setSelectedFile(event.target.files[0]);
     setIsFilePicked(true);
   };
 
-  
-  
+  const changeEmail = () => {
+    
+  }
+
+  const changePassword = () => {
+
+  }
 
   return (
     <MainWindow>
       <Heading>My Profile</Heading>
-      <Container direction={ matches ? "column" : "row" }>
-        <div className="content">{/* <DropZone /> */}</div>
-
+      <Container direction={matches ? "column" : "row"} align="flex-start">
         <Container mr="50px" mw="10vw">
           <ProfilePicture profile />
           <BasicButton
             text="Select Photo"
-            startIcon={<PhotoCameraIcon/>}
+            startIcon={<PhotoCameraIcon />}
             btnFunction={selectImage}
           />
         </Container>
         <Container align="flex-start" mw="30vw">
           <Container direction="row">
-            <TextBold mr="20px">First Name: </TextBold> <Text>{profile.firstName}</Text>
+            <TextBold mr="20px">First Name: </TextBold>{" "}
+            <Text>{profile.firstName}</Text>
           </Container>
           <Container direction="row">
-            <TextBold mr="20px" >Last Name: </TextBold> <Text>{profile.lastName}</Text>
+            <TextBold mr="20px">Last Name: </TextBold>{" "}
+            <Text>{profile.lastName}</Text>
           </Container>
           <Container direction="row">
             <TextBold mr="20px">Email: </TextBold> <Text>{profile.email}</Text>
+          </Container>
+          <Container direction="row">
+            <BasicButton text="Change Email" color="info" btnFunction={changeEmail} />
+            <BasicButton text="Change Password" color="warning" btnFunction={changePassword} />
           </Container>
         </Container>
       </Container>
