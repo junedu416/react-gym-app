b3461fed37781269b368c9ffcfe91473928fc888
ahnemui <clh.oneil@gmail.com>
2022-01-17 12:04:21 +1100

    cleans code and adds comments to events files

--- src/pages/events/NewEvent.js
+++ src/pages/events/NewEvent.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Heading, MainWindow } from "../../styled-components";
 import { createNewEvent } from "../../services/eventsServices";
